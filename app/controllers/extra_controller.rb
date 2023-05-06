7959dec2866b7ec825a56a1654b74ea81bc98d98
Egon Dohr <egondohr03@gmail.com>
2023-05-06 08:41:18 +0300

    devise and some styling

---
+++ app/controllers/extra_controller.rb
@@ -0,0 +1,5 @@
+class ExtraController < ApplicationController
+   def new
+   end 
+end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'application#hello'
   get 'signup', to: 'users#new'
   get 'goodbye', to: 'application#goodbye'
   get 'extra', to: 'application#extra'
   get 'tema', to: 'application#tema'
+  get 'logout', to: 'extra#new'
   # Defines the root path route ("/")
   # root "articles#index"
 end
